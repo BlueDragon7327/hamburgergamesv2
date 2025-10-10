fe134a8e6bded4d11e62349ff88306bd067c8618
BlueDragon <150635104+BlueDragon7327@users.noreply.github.com>
2025-10-09 20:15:50 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'offline-games-v1';
+const URLS_TO_CACHE = [
+  './',
+  './index.html',
+  './games.html',
+  './games/cubedodge.html',
+  './games/style.css'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(c => c.addAll(URLS_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
